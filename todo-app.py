f882e1ba2c84d27af641770c7f1cfc3de45fb44b
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-06 17:12:03 +0200

    created a nice view for added text

--- todo-app.py
+++ todo-app.py
@@ -1,10 +1,15 @@
 import sys
-
 def arguments():
     if len(sys.argv) == 1:
         help_printer()
     else:
         if sys.argv[1] == '-l':
+            open_database()
+        if sys.argv[1] == '-a':
+            add_line()
+        if sys.argv[1] == '-r':
+            open_database()
+        if sys.argv[1] == '-c':
             open_database()
 
 def help_printer():
@@ -13,34 +18,29 @@ def help_printer():
     print(information)
     usage_information.close()
 
-def l_argument():
-    if sys.argv[1] == '-l':
-        open_database()
-
-def a_argument():
-    if sys.argv[1] == '-a':
-        add_line()
-
-def add_line():
-    add = open('database.txt', 'a')
-    add.write("\n0;" + sys.argv[2].rstrip())
-    add.close()
-
-def r_argument():
-    if sys.argv[1] == '-r':
-        open_database()
-
-def c_argument():
-    if sys.argv[1] == '-c':
-        open_database()
-
 def open_database():
     database = open('database.txt', 'r')
     newdata = database.read()
     print(newdata)
     database.close()
 
+def add_line():
+    add = open('database.txt', 'a')
+    add.write("\n0;" + sys.argv[2].rstrip())
+    add.close()
+    view()
+
+def view():
+    viewfile = open('database.txt', 'r')
+    number = 1
+    for line in viewfile:
+        line1 = line.rstrip().split(";")
+        if line1[0] == '1':
+            print (number, "-", "[X]", line1[1])
+        else:
+            print (number,"-", "[ ]", line1[1])
+        number += 1
+    viewfile.close()
+
 arguments()
-a_argument()
-open_database()
 #print("3;Do homework".split(";"))
